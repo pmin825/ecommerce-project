da6726524b41503ba423ea9bf49ba9409ec34c84
pmin825 <pmin.0825@gmail.com>
2021-01-11 17:43:00 -0800

    add monster users

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,27 @@
-function App() {
-    return <div>hello</div>;
-}
+import React, { Component } from "react";
 
-export default App;
+export default class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            monsters: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((users) => this.setState({ monsters: users }));
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.monsters.map((monster) => (
+                    <h1 key={monster.id}> {monster.name} </h1>
+                ))}
+            </div>
+        );
+    }
+}
