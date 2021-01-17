ebbd5facbcd6f42bd2221f337174930bfbd56d33
pmin825 <pmin.0825@gmail.com>
2021-01-17 12:21:30 -0800

    add homepage component

---
+++ ecomm-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './homepage.component.jsx'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
