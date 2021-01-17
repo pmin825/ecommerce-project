e6926ca63cde5ee1ad1a21bd1e7453f18dbf2421
pmin825 <pmin.0825@gmail.com>
2021-01-16 17:53:04 -0800

    add images name and email to listing

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./card.styles.css";
 
-export const Card = (props) => (
-    <div>
-        <h1>{props.monster.name}</h1>
+export const Card = ({ monster }) => (
+    <div className="card-container">
+        <img
+            src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+            alt="monster"
+        />
+        <h2>{monster.name}</h2>
+        <p>{monster.email}</p>
     </div>
 );
