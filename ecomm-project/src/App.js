ac313f317400fc351f457e6aef1e54279868d210
pmin825 <pmin.0825@gmail.com>
2021-01-25 17:15:45 -0800

    updating app with shop page, collection preview component, and collection item component

--- ecomm-project/src/App.js
+++ ecomm-project/src/App.js
@@ -5,14 +5,14 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/shop" component={ShopPage} />
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
