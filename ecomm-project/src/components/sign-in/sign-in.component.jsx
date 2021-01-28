f4c683554bd7345c4458d0e020d0e623ab1ee716
pmin825 <pmin.0825@gmail.com>
2021-01-27 17:53:31 -0800

    create form input component

--- ecomm-project/src/components/sign-in/sign-in.component.jsx
+++ ecomm-project/src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -27,22 +28,22 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
             required
-          ></input>
-          <label>Email</label>
-          <input
+          />
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
             required
-          ></input>
-          <label>Password</label>
+          />
           <input type="submit" value="Submit Form" />
         </form>
       </div>
