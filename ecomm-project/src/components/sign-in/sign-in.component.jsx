c4dde7b84d98d62dbe77032364606c84860f7004
pmin825 <pmin.0825@gmail.com>
2021-01-27 17:59:19 -0800

    create custom button component

--- ecomm-project/src/components/sign-in/sign-in.component.jsx
+++ ecomm-project/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -44,7 +45,9 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit" value="Submit Form">
+            Sign in
+          </CustomButton>
         </form>
       </div>
     );
