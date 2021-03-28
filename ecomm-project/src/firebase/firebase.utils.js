1e00e4797bdf36d48860472a724739a46ecca69b
pmin825 <pmin.0825@gmail.com>
2021-03-28 16:02:13 -0700

    create cart icon and cart dropdown

--- ecomm-project/src/firebase/firebase.utils.js
+++ ecomm-project/src/firebase/firebase.utils.js
@@ -3,38 +3,38 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyDLeg_RuopKdivd5avtCkpVgL6FcOJJkBI",
-  authDomain: "ecomm-project-db.firebaseapp.com",
-  projectId: "ecomm-project-db",
-  storageBucket: "ecomm-project-db.appspot.com",
-  messagingSenderId: "320666087980",
-  appId: "1:320666087980:web:28ab7f12293e85aa435789",
+    apiKey: "AIzaSyDLeg_RuopKdivd5avtCkpVgL6FcOJJkBI",
+    authDomain: "ecomm-project-db.firebaseapp.com",
+    projectId: "ecomm-project-db",
+    storageBucket: "ecomm-project-db.appspot.com",
+    messagingSenderId: "320666087980",
+    appId: "1:320666087980:web:28ab7f12293e85aa435789",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get();
+    const snapShot = await userRef.get();
 
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
     }
-  }
 
-  return userRef;
+    return userRef;
 };
 
 firebase.initializeApp(config);
